f6cedab378ac9eea2c2e0484b8668934f9bfc013
Siarhei Kukunin <siarhei.kukunin@zealicsolutions.com>
2024-04-14 14:01:11 +0200

    feat: hide sensitive data in logs

--- libs/common/src/middlewares/logger/index.ts
+++ libs/common/src/middlewares/logger/index.ts
@@ -11,10 +11,12 @@ export class LoggerMiddleware implements NestMiddleware {
     const { ip, method, originalUrl } = request;
 
     const userAgent = request.get('user-agent') || '';
-    const body = JSON.stringify(request.body);
+
+    const body = this.maskSensitiveDate({ ...request.body });
+    const bodyStr = JSON.stringify(body);
 
     this.logger.log(
-      `Request: ${method} ${originalUrl}, ip=${ip}, user-agent=${userAgent} body=${body}`,
+      `Request: ${method} ${originalUrl}, ip=${ip}, user-agent=${userAgent} body=${bodyStr}`,
     );
 
     response.on('close', () => {
@@ -25,5 +27,17 @@ export class LoggerMiddleware implements NestMiddleware {
     });
 
     next && next();
+  }
+
+  maskSensitiveDate(body: Record<string, any>): Record<string, any> {
+    if ('email' in body) {
+      body['email'] = body['email'].replace(/^(\w)(?:\w*?)(\w?)@/i, '$1***$2@');
+    }
+
+    if ('password' in body) {
+      body['password'] = '***';
+    }
+
+    return body;
   }
 }
