d4ad642c9016b404b34464f8b7b4074b9278adbc
Siarhei Kukunin <siarhei.kukunin@zealicsolutions.com>
2024-04-17 02:54:44 +0200

    feat: add docker-compose

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import sassDts from 'vite-plugin-sass-dts'
+import sassDts from "vite-plugin-sass-dts";
 
 export default defineConfig({
   plugins: [react(), sassDts()],
   server: {
+    host: "0.0.0.0",
     port: 3000,
   },
-})
+});
